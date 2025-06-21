1a8428ef0e811b30c8a963694856931c7f48e7c7
Bo Lei <bo@fleak.ai>
2025-06-22 08:03:44 +0900

    try to fix broken test

--- tests/zephflow_test.py
+++ tests/zephflow_test.py
@@ -1,9 +1,14 @@
+from pathlib import Path
+
 from zephflow import ZephFlow
 
 
 class TestZephFlowIntegration:
     def test_process_from_yaml_dag(self):
-        with open("resources/test_dag.yaml", "r") as file:
+        test_dir = Path(__file__).parent
+        yaml_file_path = test_dir / "resources" / "test_dag.yaml"
+
+        with open(yaml_file_path, "r") as file:
             file_content = file.read()
         print(file_content)
         flow = ZephFlow.from_yaml_dag(file_content)
